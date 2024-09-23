5f3826eec7040c8bcc37bddf19c61aa9849fca8e
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-09-23 17:34:28 -0300

    modal de criação de categoria e fornecedor em create de produto

--- src/app/produtos/create/page.tsx
+++ src/app/produtos/create/page.tsx
@@ -2,6 +2,7 @@
 
 import { Categoria, Fornecedor, Produto } from "@/app/lib/types";
 import { fetchApi } from "@/app/lib/utilities";
+import { Modal } from "@/components/page";
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm, useWatch } from "react-hook-form";
 
@@ -9,6 +10,11 @@ export default function CreateProduto() {
     const { register, control, handleSubmit, reset, formState: { errors } } = useForm<Produto>();
     const [categorias, setCategorias] = useState<Categoria[]>([])
     const [fornecedores, setFornecedores] = useState<Fornecedor[]>([])
+    const [open, setOpen] = useState(false)
+    const [openCat, setOpenCat] = useState(false)
+
+    const { register: registerFornecedor, handleSubmit: handleSubmitFornecedor, reset: resetFornecedor, formState: { errors: errorsFornecedor } } = useForm<Fornecedor>();
+    const { register: registerCategoria, handleSubmit: handleSubmitCategoria, reset: resetCategoria, formState: { errors: errorsCategoria } } = useForm<Categoria>();
 
     const onSubmit: SubmitHandler<Produto> = (data) => {
         fetch('https://localhost:44334/api/produto/', {
@@ -29,6 +35,52 @@ export default function CreateProduto() {
         .then((data) => {
             reset();
             //router.replace('/home');
+        });
+    };
+
+    const onSubmitFornecedor: SubmitHandler<Fornecedor> = (data) => {
+        fetch('https://localhost:44334/api/fornecedor/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                ...data
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Falha ao enviar os dados para API');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            resetFornecedor();
+            setOpen(false);
+            getFornecedores();
+        });
+    };
+
+    const onSubmitCategoria: SubmitHandler<Categoria> = (data) => {
+        fetch('https://localhost:44334/api/categoria/', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                ...data
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Falha ao enviar os dados para API');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            resetCategoria();
+            setOpenCat(false);
+            getCategorias();
         });
     };
 
@@ -53,33 +105,96 @@ export default function CreateProduto() {
         name: "fornecedorId"
     });
 
-    useEffect(() => {
-        const getCategorias = async () => {
-            try {
-                const categorias = await fetchApi('https://localhost:44334/api/categoria');
-                setCategorias(categorias);
-            } catch (error) {
-                console.error("Erro ao buscar categorias:", error);
-            }
-        };
+    const fornecedorNome = useWatch({
+        control,
+        name: "nome"
+    });
 
-        const getFornecedores = async () => {
-            try {
-                const fornecedores = await fetchApi('https://localhost:44334/api/fornecedor');
-                setFornecedores(fornecedores);
-            } catch (error) {
-                console.error("Erro ao buscar forncedores:", error);
-            }
-        };
-        
+    const categoriaNome = useWatch({
+        control,
+        name: "nome"
+    });
+
+    const getCategorias = async () => {
+        try {
+            const categorias = await fetchApi('https://localhost:44334/api/categoria');
+            setCategorias(categorias);
+        } catch (error) {
+            console.error("Erro ao buscar categorias:", error);
+        }
+    };
+
+    const getFornecedores = async () => {
+        try {
+            const fornecedores = await fetchApi('https://localhost:44334/api/fornecedor');
+            setFornecedores(fornecedores);
+        } catch (error) {
+            console.error("Erro ao buscar forncedores:", error);
+        }
+    };
+
+    useEffect(() => {
         getFornecedores();
         getCategorias();
     }, []);
 
     return(
         <div className="w-2/5 gap-8 m-auto mt-32 bg-blue-100 p-10 rounded-xl">
-            <h1 className="font-semibold text-xl mb-5">Criar Produto</h1>
+            
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <form  onSubmit={handleSubmitFornecedor(onSubmitFornecedor)}>
+                    <div className="mb-4">
+                        <label className="block text-gray-800 text-sm font-bold mb-1" htmlFor="fornecedorNome">Nome do fornecedor:</label>
+                        <input
+                            type="text"
+                            {...registerFornecedor('nome', { required: 'Nome é obrigatório' })}
+                            className="w-full px-3 py-2 border rounded-md text-gray-700 font-sans focus:outline-none"
+                        />
+                        {errorsFornecedor.nome && <p className="text-red-500 text-xs italic mt-1">{errorsFornecedor.nome.message}</p>}
+                    </div>
 
+                    <button 
+                        type="submit"
+                        className="bg-blue-600 hover:bg-blue-700 text-white w-full text-center py-3 rounded-md mt-5"
+                    >
+                        Adicionar Fornecedor
+                    </button>
+                </form>
+            </Modal>
+
+            <Modal open={openCat} onClose={() => setOpenCat(false)}>
+                <form  onSubmit={handleSubmitCategoria(onSubmitCategoria)}>
+                    <div className="mb-4">
+                        <label className="block text-gray-800 text-sm font-bold mb-1" htmlFor="categoriaNome">Nome da categoria:</label>
+                        <input
+                            type="text"
+                            {...registerCategoria('nome', { required: 'Nome é obrigatório' })}
+                            className="w-full px-3 py-2 border rounded-md text-gray-700 font-sans focus:outline-none"
+                        />
+                        {errorsCategoria.nome && <p className="text-red-500 text-xs italic mt-1">{errorsCategoria.nome.message}</p>}
+                    </div>
+
+                    <button 
+                        type="submit"
+                        className="bg-blue-600 hover:bg-blue-700 text-white w-full text-center py-3 rounded-md mt-5"
+                    >
+                        Adicionar Categoria
+                    </button>
+                </form>
+            </Modal>
+
+            <div className="flex items-center justify-between">    
+                <h1 className="font-semibold text-xl mb-5">Criar Produto</h1>
+                <div className="flex items-center gap-2">
+                    <button onClick={() => setOpenCat(true)} className="bg-green-500 text-white flex justify-center items-center p-2 rounded-md">
+                        Criar Categoria
+                    </button>
+                    <button onClick={() => setOpen(true)} className="bg-green-500 text-white flex justify-center items-center p-2 rounded-md">
+                        Criar Fornecedor
+                    </button>
+                </div>
+            </div>
+            
             <form onSubmit={handleSubmit(onSubmit)} >
                 <div className="mb-4">
                     <label className="block text-gray-800 text-sm font-bold mb-1" htmlFor="nome">Nome:</label>
